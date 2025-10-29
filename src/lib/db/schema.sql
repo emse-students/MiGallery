a227697697e1151bc779b460451346be42cc86d7
j0lan <jolan.boudin.jourdan@gmail.com>
2025-10-30 00:20:43 +0100

    Feature: Introduce SQLite database integration

---
+++ src/lib/db/schema.sql
@@ -0,0 +1,13 @@
+-- Schéma de la base de données MiGallery
+
+CREATE TABLE IF NOT EXISTS users (
+    id_user TEXT PRIMARY KEY,
+    email TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    nom TEXT NOT NULL,
+    id_photos TEXT NOT NULL
+);
+
+-- Insertion de vos données (uniquement si pas déjà présent)
+INSERT OR IGNORE INTO users (id_user, email, prenom, nom, id_photos) 
+VALUES ('jolan.boudin', 'jolan.boudin@etu.emse.fr', 'Jolan', 'BOUDIN', '031b39ea-7c35-4d52-bc00-46ff0d927afa');
