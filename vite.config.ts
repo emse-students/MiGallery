6176222e917b767a903d1e6778b8881e83cf0cc4
j0lan <jolan.boudin.jourdan@gmail.com>
2025-11-17 20:17:27 +0100

    gougougagak

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    resolve: {
-        conditions: ['browser']
-    },
-    build: {
-        minify: 'esbuild',
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    if (id.includes('node_modules')) {
-                        return 'vendor';
-                    }
-                }
-            }
-        }
-    },
     preview: {
         host: '0.0.0.0',                 
         port: 4173,                      
